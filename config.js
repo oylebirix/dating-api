979604d1e96e53b4fe580767d06660937d33e32a
Cedricmy <mmt.plt.1905@gmail.com>
2023-12-25 20:44:01 +0300

    first commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    MONGO_DB_URL:"mongodb://localhost:27017/koddemo",
+    MONGO_DB_URL:"mongodb://localhost:27017/Chatme",
     MONGO_DB_URL_DEBUG:"",
     PORT:process.env.PORT || 3300,
     PRIVATE_KEY:"56454743358713353",
@@ -7,11 +7,11 @@ const config = {
     MIN_PASSWORD_LENGTH:6,
     AUTHORIZATION:"Bearer ",
     PROD:false,
-    ONE_SIGNAL_ID:"1ca8e627-83e7-4765-910e-cd6505b6441c",
-    ONE_SIGNAL_SECRET:"MmQ3NDI2ZTctYWZjYy00M2RjLTk1NzUtNGE4ZGE1MGY2NThk",
-    HOST:"http://188.132.130.77:3300",
-    APP_NAME:"Fist Date",
-    EMAIL: "cmpolatqx@gmail.com",
-    AUTHOR:"Fist Date",
+    ONE_SIGNAL_ID:"5702b6c3-b860-41a4-a652-84d8e232d126",
+    ONE_SIGNAL_SECRET:"NWVhNDYyMGMtNzQ3Mi00OTE2LTllYzMtNWVjMWQxMGJlZTg0",
+    HOST:"http://45.147.46.119:3300",
+    APP_NAME:"Chat Me",
+    EMAIL: "chatme@gmail.com",
+    AUTHOR:"ChatMe",
 }
 module.exports = config
